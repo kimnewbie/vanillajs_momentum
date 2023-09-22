b22fcf3e82a51d5eca089c8d0148dc7b227c9d0b
youjin kim <gracecancho@gmail.com>
2023-09-22 13:36:51 +0900

    CSS in Javascript

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-    h1.style.color = "orange";
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if (currentColor === 'orange') {
+        newColor = "blue";
+    } else {
+        newColor = "orange";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here :)"
-}
 
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone :'("
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert('copier!')
-}
-
-function handleWindowOfflline() {
-    alert('SOS no WIFI')
-}
-
-function handleWindowOnlline() {
-    alert('ALL GOOD!')
-}
-
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("onmouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize); // 크기 변경
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOfflline);
-window.addEventListener("online", handleWindowOnlline);  
+h1.addEventListener("click", handleTitleClick); 
