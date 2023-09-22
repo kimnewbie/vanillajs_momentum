ff8e7442d46053268648f8154e539774a1609be4
youjin kim <gracecancho@gmail.com>
2023-09-22 15:48:21 +0900

    string.padStart를 이용하여 1자리수를 2자리수로 자리 맞춤하기

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
+// 시계가 자릿수 2자리씩으로 정하기
+// String() or ().toString()
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 setInterval(getClock, 1000)
