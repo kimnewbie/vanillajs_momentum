ddc592ecf24ce405f39a4837847a9ac30caf812e
youjin kim <gracecancho@gmail.com>
2023-09-22 13:05:20 +0900

    Searching For Elements

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const title = document.getElementById("title");
-title.innerHTML = 'Got you!'
+const hellos = document.getElementsByClassName('hello')
+// const title = document.getElementsByTagName('h1');
+const title = document.querySelectorAll('.hello h1');
+const firstChild = document.querySelector('.hello h1:first-child');
 
-console.dir(title)
+const findById = document.getElementById("hello");
+const findById2 = document.querySelector("#hello");
 
-console.log(title.id)
-console.log(title.className)
+console.log(hellos)
+console.log(title)
+console.log(firstChild)
+console.log(findById)
+console.log(findById2)
