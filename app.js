68d0433455c3d51f04241e1a1004735124b99a6a
youjin kim <gracecancho@gmail.com>
2023-09-22 10:18:38 +0900

    isNaN

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const age = prompt("How old are you?"); // 페이지를 멈추게해서 더이상 사용하지 않아
 
-console.log(age)
+if (isNaN(age)) {
+    console.log("Please write a number")
+} else {
+    console.log("Thank you for writing your age")
+}
