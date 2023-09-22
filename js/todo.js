d1068c129f3ce0a34fc14b7d2a5194f51c88deae
youjin kim <gracecancho@gmail.com>
2023-09-22 17:50:16 +0900

    delete 기능 보완

--- js/todo.js
+++ js/todo.js
@@ -2,7 +2,7 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+let toDos = []; // const 로 하면 기능 작동 안함
 const TODOS_KEY = 'todos'
 
 function saveToDos() {
@@ -14,12 +14,15 @@ function deleteToDo(event) {
     // event에서 parentNode : li 를 찾을 수 있어
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id; // 삭제를 위해 id 추가
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.todo;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener('click', deleteToDo);
@@ -32,8 +35,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        todo: newTodo,
+        id: Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
@@ -43,5 +50,5 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach((item) => console.log(item));
+    parsedToDos.forEach(paintTodo);
 }
