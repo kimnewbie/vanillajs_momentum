dcb895b920fd368514cac105418ff54d90e7cf25
youjin kim <gracecancho@gmail.com>
2023-09-22 14:20:01 +0900

    input 길이 제한 조건문보다 기존 기능 활용하기

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.log('clicked!')
+    const username = loginInput.value;
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
