dc7e5344925311edd383aaa1a41335b1f50c0bcf
youjin kim <gracecancho@gmail.com>
2023-09-22 15:03:05 +0900

    submit 하면 welcome 메세지 뜨게 hidden 수정

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector('#greeting');
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event)
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello  + ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-    event.preventDefault();
-}
-
-loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', handleLinkClick);
+loginForm.addEventListener('submit', onLoginSubmit); 
