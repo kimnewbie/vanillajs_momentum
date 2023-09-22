7ed2e7cfabc9e588507cf9bb63b7abed50d7f7a1
youjin kim <gracecancho@gmail.com>
2023-09-22 15:24:14 +0900

    localStorage 사용

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로 표기
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = `Hello  + ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username); // add localStorage 
+    paintGreetings(username);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit); 
+function paintGreetings(username) {
+    greeting.innerText = `Hello, ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    // show the greetings 
+    paintGreetings(savedUsername);
+}
