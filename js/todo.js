cedec5ab621dfa39e7d9a7e2752b0a07ed198eaf
youjin kim <gracecancho@gmail.com>
2023-09-22 17:16:19 +0900

    adding delete button and the function

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    // event에서 parentNode : li 를 찾을 수 있어
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span); // <li><span></span></li>
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener('click', deleteToDo);
+    li.appendChild(span); // <li><span></span></li>
+    li.appendChild(button);
     toDoList.appendChild(li); // todo list 목록
 }
 
