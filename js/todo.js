6b710c737d96fef47e925c2ea0ac3a536c5d5662
youjin kim <gracecancho@gmail.com>
2023-09-22 17:30:17 +0900

    localStorage에 있는 todo list 가져오기

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,12 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
 const toDos = [];
+const TODOS_KEY = 'todos'
 
 function saveToDos() {
-    // localStorage.setItem('todos', toDos); 이렇게하면 []로 저장이 안돼
-    localStorage.setItem('todos', JSON.stringify(toDos));
+    // localStorage.setItem(TODOS_KEY, toDos); 이렇게하면 []로 저장이 안돼
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 };
-
 
 function deleteToDo(event) {
     // event에서 parentNode : li 를 찾을 수 있어
@@ -38,3 +38,10 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log(item));
+}
