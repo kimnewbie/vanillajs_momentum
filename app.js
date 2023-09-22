0adac89a0888c103a21e063bcca0f8341666bb0a
youjin kim <gracecancho@gmail.com>
2023-09-22 13:57:20 +0900

    클릭하면 기존 h1 className이 사라지는 현상

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const h1 = document.querySelector('div.hello:first-child h1');
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-
-    if (currentColor === 'orange') {
-        newColor = "blue";
+    const clickedClass = "clicked"
+    if (h1.className === clickedClass) {
+        h1.className = ''
     } else {
-        newColor = "orange";
+        h1.className = clickedClass
     }
-    h1.style.color = newColor;
 }
-
 
 h1.addEventListener("click", handleTitleClick); 
