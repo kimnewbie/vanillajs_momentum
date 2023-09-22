e9c6e9cbc66e50ba1c3a676080ab090d04026f94
youjin kim <gracecancho@gmail.com>
2023-09-22 17:08:38 +0900

    todo list에 li 생성하기

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span); // <li><span></span></li>
+    span.innerText = newTodo;
+    toDoList.appendChild(li); // todo list 목록
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
